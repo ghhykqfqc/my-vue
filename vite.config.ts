2f3befbd69f2bc35535cbda9397d6e0644f9144f
HYongkang <hyk8899@mail.notes.bank-of-china.com>
2024-12-02 15:43:00 +0800

    配置 tsconfig.json 用于定义编译选项和项目结构

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,33 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+  server: {
+    port: 8080,
+    // proxy: {
+    //   // string shorthand:
+    //   // http://localhost:5173/foo
+    //   //   -> http://localhost:4567/foo
+    //   '/foo': 'http://localhost:4567',
+    //   // with options:
+    //   // http://localhost:5173/api/bar
+    //   //   -> http://jsonplaceholder.typicode.com/bar
+    //   '/api': {
+    //     target: 'http://jsonplaceholder.typicode.com',
+    //     changeOrigin: true,
+    //     rewrite: (path) => path.replace(/^\/api/, ''),
+    //   },
+    // }
+  },
+  build: {
+    outDir: 'dist',
+  },
 })
